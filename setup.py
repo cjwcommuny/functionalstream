254db7a85a7c395abd4b6cb8f6f332777b9f0ece
cjwcommuny <cjwcommuny@outlook.com>
2020-07-19 11:03:11 +0800

    add PathStream

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="functionalstream", # Replace with your own username
-    version="0.1.6",
+    version="0.1.7",
     author="cjwcommuny",
     author_email="cjwcommuny@outlook.com",
     description="A functional interface for python iterator",
